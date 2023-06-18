5e40811f294d2c72c9c41955f621a89cca8c6c66
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-06-18 15:44:19 -0400

    player images assigned

--- endgame.js
+++
@@ -1,5 +0,0 @@
-//reset button function to clear board
-//arrays of winning combinations
-//for loop of winning combinations to determine winner each turn
-//if player hits winning array, display modal box "*player name* sucks less " , else allow next user to go 
-//if all boxes are filled, display modal box "You both suck"
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let playerOne = "X"
-let playerTwo = "O"
+let playerOne = "O"
+let playerTwo = "X"
 let currentPlayer = 1
 const grid = document.getElementById('grid')
 const cellElements = document.querySelectorAll('.cell');
@@ -14,17 +14,33 @@ const restartButton = document.getElementById('restartButton')
 // ]
 let board = [0,0,0,0,0,0,0,0,0]
 
-function clearGame (){
+function clearGame (restartButton){
     currentPlayer = 1
     board = [0,0,0,0,0,0,0,0,0]
+    restartButton.document.addClickListeners('click', () => {
+        img.remove();
+    })
 }
 
-function isWinningMove (index){
-    if (index === 7){
-        if (board[1]===board[7] && board[4] ===board[7] ) {
+function isWinningMove(index){
+
+    if (index === 0){
+        if (board[3]===board[0] && board[6]===board[0]){
             return true
         }
-        if (board[6]=== board[7] && board[8]===board[7]){
+        if (board[1]===board[0] && board[2]===board[0]){
+            return true
+        }
+        if (board[4]===board[0] && board[8]===board[0]){
+            return true
+        }
+    }
+
+    if (index === 1){
+        if (board[0]===board[1] && board[2]===board[1] ){
+            return true
+        }
+        if (board[4]=== board[1] && board[7]=== board [1]){
             return true
         }
     }
@@ -39,14 +55,85 @@ function isWinningMove (index){
         if (board[4]===board[2] && board[6]===board[2]){
             return true
         }
-        
+    }
+
+    if (index === 3){
+        if (board[0]===board[3] && board[6]===board[3]){
+            return true
+        }
+        if (board[4]===board[3] && board[5]===board[3]){
+            return true
+        }
+    }
+
+    if (index === 4){
+        if (board[1]===board[4] && board[7]===board[4]){
+            return true
+        }
+        if (board[3]===board[4]&& board[5]===board[4]){
+            return true
+        }
+    }
+
+    if (index === 5){
+        if (board[2]===board[5]&& board[8]===board[5]){
+            return true
+        }
+        if (board[3]===board[5] && board[4]===board[5]){
+            return true
+        }
+    }
+    
+    if (index === 6){
+        if (board[0]===board[6]&& board[3]===board[6]){
+            return true
+        }
+        if (board[7]===board[6]&& board[8]===board[6]){
+            return true
+        }
+        if (board[4]===board[6] && board[2]===board[6]){
+            return true
+        }
+    }
+
+    if (index === 7){
+        if (board[1]===board[7] && board[4] ===board[7]){
+            return true
+        }
+        if (board[6]=== board[7] && board[8]===board[7]){
+            return true
+        }
     } 
+
+    if (index === 8){
+        if (board[6]===board[8] && board[7]===board[8]){
+            return true
+        }
+        if (board[2]===board[8] && board[5]===[8]){
+            return true
+        }
+        if (board[0]===board[8] && board[4]===board[8]){
+            return true
+        }
+    }
     return false
 }
 
-function updateUi() {
-
+function image() {
+    let img = document.createElement('img');
+    img.src = "assets/letterX.png";
+    document.body.append(img);
 }
+
+function newImage() {
+    let img = document.createElement('img');
+    img.src = "assets/letterO.png";
+    document.body.append(img);
+}
+
+//function updateUI(image){}
+
+
 
 function alertPlayerWon(playerNumber) {
     console.log(playerNumber, "won") 
@@ -59,7 +146,7 @@ function addClickListeners(element, index) {
             // make the move
             board[index] = currentPlayer
             // update the ui
-            updateUi()
+            //updateUi()
             console.log(board)
             const isWinner = isWinningMove(index)
     
@@ -75,7 +162,7 @@ function addClickListeners(element, index) {
                 }
                 }    
         } else { 
-            alert("choose another")
+            alert("Pick another box")
 
         // if not a valid move:
             // send alert to player to choose another square
@@ -91,13 +178,10 @@ cellElements.forEach(addClickListeners)
 function placeMark(index) {
     console.log(index)
 }
-//for ( let i = 0; i<cellElements.length; ){
-    //  console.log(index); i++
-// }
-
-
-//for (let i = 0; i <= 9; i+++) {
- //  document.getElementById(i.toString()).addEventListener(
-    //"click",
- //function()
     
+
+
+
+image ('assets/letterO.png');
+newImage ('assets/letterX.png');
+
